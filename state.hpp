74267092a10120e9430ea86596118973b5091c09
Laurent Michel <ldm@engr.uconn.edu>
2018-09-11 21:29:49 -0400

    added missing file

---
+++ state.hpp
@@ -0,0 +1,17 @@
+#ifndef __STATE_H
+#define __STATE_H
+
+#include <functional>
+#include "handle.hpp"
+
+class StateManager {
+public:
+   StateManager() {}
+   virtual ~StateManager() {}
+   typedef handle_ptr<StateManager> Ptr;
+   virtual void saveState() = 0;
+   virtual void restoreState() = 0;
+   virtual void withNewState(std::function<void(void)>& body) = 0;
+};
+
+#endif
