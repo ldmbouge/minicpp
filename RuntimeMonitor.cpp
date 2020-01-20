3efba80782b5c660e50b137a487116c9614f303a
Laurent Michel <laurent.michel@uconn.edu>
2020-01-20 11:27:58 -0500

    runtime

--- RuntimeMonitor.cpp
+++ RuntimeMonitor.cpp
@@ -8,6 +8,27 @@
 
 #include "RuntimeMonitor.hpp"
 
+struct timeval timeval_subtract(struct timeval* x,struct timeval* y) 
+{
+   /* Perform the carry for the later subtraction by updating y. */
+   if (x->tv_usec < y->tv_usec) {
+      int nsec = (y->tv_usec - x->tv_usec) / 1000000 + 1;
+      y->tv_usec -= 1000000 * nsec;
+      y->tv_sec += nsec;
+   }
+   if (x->tv_usec - y->tv_usec > 1000000) {
+      int nsec = (x->tv_usec - y->tv_usec) / 1000000;
+      y->tv_usec += 1000000 * nsec;
+      y->tv_sec -= nsec;
+   }
+   
+   /* Compute the time remaining to wait.
+    tv_usec is certainly positive. */
+   struct timeval result;
+   result.tv_sec = x->tv_sec - y->tv_sec;
+   result.tv_usec = x->tv_usec - y->tv_usec;
+   return result;
+}
 
 long RuntimeMonitor::cputime()
 {
--- RuntimeMonitor.hpp
+++ RuntimeMonitor.hpp
@@ -13,26 +13,7 @@
 #include <sys/time.h>
 #include <sys/resource.h>
 
-struct timeval timeval_subtract(struct timeval* x,struct timeval* y) {
-   /* Perform the carry for the later subtraction by updating y. */
-   if (x->tv_usec < y->tv_usec) {
-      int nsec = (y->tv_usec - x->tv_usec) / 1000000 + 1;
-      y->tv_usec -= 1000000 * nsec;
-      y->tv_sec += nsec;
-   }
-   if (x->tv_usec - y->tv_usec > 1000000) {
-      int nsec = (x->tv_usec - y->tv_usec) / 1000000;
-      y->tv_usec += 1000000 * nsec;
-      y->tv_sec -= nsec;
-   }
-   
-   /* Compute the time remaining to wait.
-    tv_usec is certainly positive. */
-   struct timeval result;
-   result.tv_sec = x->tv_sec - y->tv_sec;
-   result.tv_usec = x->tv_usec - y->tv_usec;
-   return result;
-}
+struct timeval timeval_subtract(struct timeval* x,struct timeval* y);
 
 class RuntimeMonitor {
 public:
@@ -42,7 +23,5 @@ public:
    static timeval now();
    static timeval  elapsedSince(timeval then);
 };
-
-
-
 #endif /* RuntimeMonitor_hpp */
+
