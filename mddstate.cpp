b76006737fa1b85724314bba1845d5e774330963
Willem van Hoeve <vanhoeve@andrew.cmu.edu>
2020-03-13 14:08:45 -0400

    sumMDD updates

--- mddstate.cpp
+++ mddstate.cpp
@@ -406,26 +406,30 @@ namespace Factory {
       // Enforce
       //   sum(i, array[i]*vars[i]) >= lb and
       //   sum(i, array[i]*vars[i]) <= ub
-      spec.append(vars);
-    
+      mdd.append(vars);
+     
       auto& d = mdd.makeConstraintDescriptor(vars,"sumMDD");
 
       // Define the states: minimum and maximum weighted value (initialize at 0, maximum is ub).
-      // The "rem" state (for capturing remaining value) is not effective -- instead, use bottom-up state information.
+      // The 'rem' state (for capturing remaining value) is not effective -- instead, use bottom-up state information (when available).
       const int minW = mdd.addState(d, 0, ub);
       const int maxW = mdd.addState(d, 0, ub);
 
-      // State 'len' is needed to capture the index i of the variable array, to express array[i]*val when variable vars[i]=val.
-      const int len  = mdd.addState(d,0,vars.size());
+      // State 'len' is needed to capture the index i, to express array[i]*val when vars[i]=val.
+      const int len  = mdd.addState(d, 0, vars.size());
 
+      // The lower bound needs the bottom-up state information to be effective.
       mdd.addArc(d,[=] (const auto& p, const auto& c, var<int>::Ptr var, int val) -> bool {
-	  return (p.at(minW) + val*array[len] <= ub) &&
-	         (p.at(maxW) + val*array[len] >= lb);
+	    return (p.at(minW) + val*array[p.at(len)] <= ub);// &&
+	    //	         (p.at(maxW) + val*array[len] >= lb);
       });
 
-      mdd.addTransition(minW,[minW,array,len] (auto& out,const auto& p,auto var, int val) { out.set(minW, p.at(minW) + array[len]*val);});
-      mdd.addTransition(maxW,[maxW,array,len] (auto& out,const auto& p,auto var, int val) { out.set(maxW, p.at(maxW) + array[len]*val);});
-      mdd.addTransition(len, [len]            (auto& out,const auto& p,auto var, int val) { out.set(len,  p.at(len) + 1);});      
+      mdd.addTransition(minW,[minW,array,len] (auto& out,const auto& p,auto var, int val) {
+	  out.set(minW, p.at(minW) + array[p.at(len)]*val);});
+      mdd.addTransition(maxW,[maxW,array,len] (auto& out,const auto& p,auto var, int val) {
+	  out.set(maxW, p.at(maxW) + array[p.at(len)]*val);});
+      mdd.addTransition(len, [len]            (auto& out,const auto& p,auto var, int val) {
+	  out.set(len,  p.at(len) + 1);});      
 
       mdd.addRelaxation(minW,[minW](auto& out,const auto& l,const auto& r) { out.set(minW,std::min(l.at(minW), r.at(minW)));});
       mdd.addRelaxation(maxW,[maxW](auto& out,const auto& l,const auto& r) { out.set(maxW,std::max(l.at(maxW), r.at(maxW)));});
