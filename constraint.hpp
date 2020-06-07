97daea937f8a57adfbcf75f9520b617cce13a129
Tim Curry <timothy.curry@uconn.edu>
2020-06-07 17:08:08 -0400

    Clean up commented table code from constraint files.

--- constraint.hpp
+++ constraint.hpp
@@ -350,70 +350,6 @@ public:
    void propagate() override;
 };
 
-// class TableCT : public Constraint {
-//     // typedef std::vector<bool> boolVec;  // acts as dynamically sized bitset
-//     class Entry {
-//         const int _index;
-//         const int _min;
-//         const int _max;
-//         public:
-//           Entry(int index, int min, int max) :
-//             _index(index),
-//             _min(min),
-//             _max(max)
-//             {}
-//           int getIndex() { return _index;}
-//           int getMin() { return _min;}
-//           int getMax() {return _max;}
-//     };
-//     SparseBitSet                                        _currTable;
-//     // std::map<var<int>::Ptr, Entry>                   _entries;
-//     std::map<int, Entry>                                _entries;
-//     std::vector<std::vector<StaticBitSet>>              _supports;
-//     std::vector<std::vector<int>>                       _table;
-//     std::vector<std::vector<int>>                       _residues;
-//     std::vector<var<int>::Ptr>                          _vars;
-//     std::vector<var<int>::Ptr>                          _Sval;
-//     std::vector<var<int>::Ptr>                          _Ssup;
-//     std::vector<trail<int>>                             _lastSizes;
-//     int                                                 _sz;
-//     void                                                filterDomains();
-//     void                                                updateTable();
-// public:
-//     template <class Vec> TableCT(const Vec& x, const std::vector<std::vector<int>>& table)
-//         : Constraint(x[0]->getSolver()),
-//           _table(table),
-//           _sz(table.size()),
-//           _currTable(x[0]->getSolver()->getStateManager(), x[0]->getStore(), table.size())  // build SparseBitSet for vectors in table
-//     {
-//         int currIndex = 0;
-//         for (const auto& vp : x) {
-//             _vars.push_back(vp);
-//             _entries.emplace(vp->getId(), Entry(currIndex, vp->min(), vp->max()));  // build entries for each var
-//             // trail<int> t(x[0]->getSolver()->getStateManager(), vp->size());  // record initial var domain size
-//             _lastSizes.push_back(trail<int>(x[0]->getSolver()->getStateManager(), vp->size()));  // record initial var domain size
-//             _supports.emplace_back(std::vector<StaticBitSet>(0));  // init vector of bitsets for var's supports
-//             _residues.emplace_back(std::vector<int>());
-//             std::vector<StaticBitSet>& v = _supports.back();
-//             std::vector<int>& r = _residues.back();
-//             for (int val=vp->min(); val < vp->max()+1; val++) {
-//                 v.emplace_back(StaticBitSet(table.size()));  // make bitset for this value
-//                 // loop through table to see which table vectors support this value
-//                 for (int i=0; i < table.size(); i++) {
-//                     const std::vector<int>& tableEntry = table[i];
-//                     if (tableEntry[currIndex] != val)
-//                         v.back().remove(i);  // this tableEntry is not a support for val
-//                 }
-//                 r.push_back(_currTable.intersectIndex(v.back()));
-//             }
-//             currIndex++;
-//         }
-//     }
-//     ~TableCT() {}
-//     void post() override;
-//     void propagate() override;
-// };
-
 namespace Factory {
    inline Constraint::Ptr equal(var<int>::Ptr x,var<int>::Ptr y,int c=0) {
       return new (x->getSolver()) EQBinBC(x,y,c);
@@ -626,9 +562,6 @@ namespace Factory {
       y->getSolver()->post(new (y->getSolver()) Element1DVar(flat,y,z));
       return z;
    }
-   // template <class Vec, class T> Constraint::Ptr table(const Vec& x, const T& table) {
-   //    return new (x[0]->getSolver()) TableCT(x, table);
-   // }
 };
 
 void printCstr(Constraint::Ptr c); 
