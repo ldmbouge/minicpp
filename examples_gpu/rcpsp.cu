209612afbcb38dbea39516f2dd4cc9bc75adb78e
Fabio Tardivo <tardivo.fabio@gmail.com>
2026-01-24 15:56:09 -0500

    Add RCPSP Skeleton

---
+++ examples_gpu/rcpsp.cu
@@ -0,0 +1,13 @@
+
+#include <iostream>
+#include <iomanip>
+#include "solver.hpp"
+#include "trailable.hpp"
+#include "intvar.hpp"
+#include "constraint.hpp"
+#include "search.hpp"
+
+int main(int argc,char* argv[])
+{
+    return 0;
+}
